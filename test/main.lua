089de4c3513b6db1edb4e60e60a61e03d8171d47
cafehaine <kilian.guillaume@gmail.com>
2020-02-17 09:11:24 +0100

    Tweak tests

--- test/main.lua
+++ test/main.lua
@@ -1,13 +1,5 @@
 local profile = require("ultiprofile")
 
-function load_save()
-	profile.load()
-	profile.set("ABC", "DEF", "HIJ")
-	profile.save()
-	profile.load()
-	assert(profile.get("ABC", "DEF") == "HIJ")
-end
-
 local function tables_equal(t1, t2)
 	for k, v in pairs(t1) do
 		if type(v) == "table" then
@@ -23,6 +15,33 @@ local function tables_equal(t1, t2)
 	return true
 end
 
+local check_count = 0
+local check_fails = 0
+
+local function check_equals(a, b)
+	check_count = check_count + 1
+
+	local equal
+	if type(a) == "table" then
+		equal = tables_equal(a, b)
+	else
+		equal = a == b
+	end
+
+	if not equal then
+		check_fails = check_fails + 1
+		print(debug.traceback("Check failed!", 2))
+	end
+end
+
+function load_save()
+	profile.load()
+	profile.set("ABC", "DEF", "HIJ")
+	profile.save()
+	profile.load()
+	check_equals(profile.get("ABC", "DEF"), "HIJ")
+end
+
 function serialization()
 	local demo_table = {"abc", true, 1, {10, "def"}}
 
@@ -34,10 +53,10 @@ function serialization()
 	profile.save()
 
 	profile.load()
-	assert(profile.get("test", "string") == "abcdef")
-	assert(profile.get("test", "number") == 3.14)
-	assert(profile.get("test", "boolean") == true)
-	assert(tables_equal(profile.get("test", "table"), demo_table))
+	check_equals(profile.get("test", "string"), "abcdef")
+	check_equals(profile.get("test", "number"), 3.14)
+	check_equals(profile.get("test", "boolean"), true)
+	check_equals(profile.get("test", "table"), demo_table)
 end
 
 TESTS = {load_save, serialization}
@@ -45,9 +64,12 @@ TESTS = {load_save, serialization}
 function love.run()
 	return function()
 		for i, func in ipairs(TESTS) do
-			print("== Test ".. i .. " out of ".. #TESTS .. " ==")
+			check_count = 0
+			check_fails = 0
+			print("==> Test ".. i .. " out of ".. #TESTS)
 			love.filesystem.remove("profile.txt")
 			func()
+			print("==> "..check_fails.." checks failed out of "..check_count)
 		end
 
 		print("Tests ran successfully.")
